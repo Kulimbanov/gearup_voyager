c24000e64e33e6626fb7ed8ac5326106a5c09b1b
Goran Kulimbanov <kulimbanov@gmail.com>
2021-08-23 17:35:57 +0200

    dark/light mode style complete

--- resources/views/page/public.blade.php
+++ resources/views/page/public.blade.php
@@ -3,36 +3,48 @@
 @section('content')
     <div class="container">
         <section class="py-5 text-center services">
-            <div class="container">
+            <div class="services">
                 <h2 class="text-center">Bicycle Services</h2>
                 <p class="text-muted mb-5 text-center">Enjoy the ride with boosted bike performance.</p>
-                <div class="services">
-                    <div class="service silver col-sm p-3 m-auto">
-                        <div class="card p-3">
+                <div class=" d-flex flex-column flex-sm-row">
+                    <div class="service silver col-sm p-3 mx-auto my-5">
+                        <div class="card wrapper p-3">
                             <font-awesome-icon class="mx-auto my-4" icon="cog" size="4x"></font-awesome-icon>
                             <h3 class="mt-4 mb-1">Silver Pack</h3>
-                            <p class="text-muted mt-1">We recommend running your bike through these simple procedures every
-                                25 hours of ride time or as you feel they are needed.</p>
+                            <ul>
+                                <li>Проверка и штелување на брзини</li>
+                                <li>Проверка и штелување на кочници</li>
+                                <li>Центрирање на тркала</li>
+                                <li>Проверка на предна осовина</li>
+                                <li>Проверка на задна осовина</li>
+                                <li>Проверка на средна осовина</li>
+                                <li>Проверка на гарнитура од волан</li>
+                                <li>Подмачкување верига (ланец)</li>
+                                <li>Кратко пробрно возење</li>
+                                <li>Чистење на велосипедот</li>
+                            </ul>
                         </div>
-                        <a href="#" class="more text-decoration-none text-dark ">Read More</a>
+                        <!--                        <a href="#" class="more text-decoration-none text-dark ">Read More</a>-->
                     </div>
-                    <div class="service gold col-sm p-3 m-auto">
-                        <div class="card p-3">
+                    <div class="service gold col-sm p-3 m-auto mb-5">
+                        <div class="card wrapper p-3">
                             <font-awesome-icon class="mx-auto my-4" icon="cogs" size="4x"></font-awesome-icon>
                             <h3 class="mt-4 mb-1">Golden Pack</h3>
-                            <p class="text-muted mt-1">Diagnostic checkup, we’ll dial in your brakes, tune and lube your
-                                shifting to perfection, true and tension the wheels, and recharge everything.</p>
+                            {{--                            <p class="text-muted mt-1">Diagnostic checkup, we’ll dial in your brakes, tune and lube--}}
+                            {{--                                your--}}
+                            {{--                                shifting to perfection, true and tension the wheels, and recharge everything.</p>--}}
                         </div>
-                        <a href="#" class="more text-decoration-none text-dark">Read More</a>
+                        {{--                        <a href="#" class="more text-decoration-none text-dark">Read More</a>--}}
                     </div>
                     <div class="service platinum col-sm p-3 m-auto">
-                        <div class="card p-3">
+                        <div class="card wrapper p-3">
                             <font-awesome-icon class="mx-auto my-4" icon="dna" size="4x"></font-awesome-icon>
                             <h3 class="mt-4 mb-1">Platinum Pack</h3>
-                            <p class="text-muted mt-1">We’ll start by stripping your bike to the bare frame for a deep clean
-                                and inspection, then we’ll repack and rebuild it..</p>
+                            {{--                            <p class="text-muted mt-1">We’ll start by stripping your bike to the bare frame for a--}}
+                            {{--                                deep clean--}}
+                            {{--                                and inspection, then we’ll repack and rebuild it..</p>--}}
                         </div>
-                        <a href="#" class="more text-decoration-none text-dark">Read More</a>
+                        {{--                        <a href="#" class="more text-decoration-none text-dark">Read More</a>--}}
                     </div>
                 </div>
             </div>
