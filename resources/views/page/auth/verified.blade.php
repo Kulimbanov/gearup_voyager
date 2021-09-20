1991c83a3512ffcefd0c752963a99c8d28e39eb2
Goran Kulimbanov <kulimbanov@gmail.com>
2021-09-20 19:47:50 +0200

    verify-email :todo implement VerificationApiController.php

--- resources/views/page/auth/verified.blade.php
+++ resources/views/page/auth/verified.blade.php
@@ -2,6 +2,115 @@
 
 @section('content')
     <div class="container">
-        <h2 class="text-center">Thank you for verifying your email.</h2>
+        <h2 class="text-center">Thank you for verifying your email.?</h2>
+        <!-- Illustration inspiration by Dennis de Groot -->
+        <div class="pause">
+
+        </div>
+        <div class="bike">
+            <div class="part frame">
+                <div class="bar left-top"></div>
+                <div class="bar left-bottom"></div>
+                <div class="bar left"></div>
+                <div class="bar top"></div>
+                <div class="bar bottom"></div>
+                <div class="bar right"></div>
+            </div>
+            <div class="part sadle">
+                <div class="sit-here"></div>
+                <div class="sadlepen"></div>
+            </div>
+            <div class="part handlebar">
+                <div class="stem"></div>
+                <div class="connector"></div>
+                <div class="prehandle"></div>
+                <div class="handle"></div>
+            </div>
+            <div class="part pedals">
+                <div class="inside"></div>
+                <div class="outside"></div>
+                <div class="pedalstem front">
+                    <div class="pedalbase front"></div>
+                </div>
+                <div class="pedalstem back">
+                    <div class="pedalbase back"></div>
+                </div>
+            </div>
+            <div class="part wheel left">
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+            </div>
+            <div class="part wheel right">
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+            </div>
+            <div class="part axis left"></div>
+            <div class="part axis right"></div>
+        </div>
+
+        <div class="bike bike-two">
+            <div class="part frame">
+                <div class="bar left-top"></div>
+                <div class="bar left-bottom"></div>
+                <div class="bar left"></div>
+                <div class="bar top"></div>
+                <div class="bar bottom"></div>
+                <div class="bar right"></div>
+            </div>
+            <div class="part sadle">
+                <div class="sit-here"></div>
+                <div class="sadlepen"></div>
+            </div>
+            <div class="part handlebar">
+                <div class="stem"></div>
+                <div class="connector"></div>
+                <div class="prehandle"></div>
+                <div class="handle"></div>
+            </div>
+            <div class="part pedals">
+                <div class="inside"></div>
+                <div class="outside"></div>
+                <div class="pedalstem front">
+                    <div class="pedalbase front"></div>
+                </div>
+                <div class="pedalstem back">
+                    <div class="pedalbase back"></div>
+                </div>
+            </div>
+            <div class="part wheel left">
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+            </div>
+            <div class="part wheel right">
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+                <div class="spoke"></div>
+            </div>
+            <div class="part axis left"></div>
+            <div class="part axis right"></div>
+        </div>
     </div>
 @endsection
